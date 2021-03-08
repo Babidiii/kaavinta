6f5218aa3834d9007e3e5b41fa0e835b15a810f5
Babidiii <bilal.guillaumie@etu.uca.fr>
2021-03-08 03:19:30 +0100

    fix: clean added to script

--- retro-song/script.sh
+++ retro-song/script.sh
@@ -18,21 +18,37 @@ download(){
    if ! youtube-dl -q -i --extract-audio -f 140  -o "${tmp_dir}/%(title)s.%(ext)s" "${link}" ; then
      echo "[ytb-dl](fail) ${title} --> ${link}" >> errors.log
    fi
-  done <<< $( tail -n +2 ${file} )v
+  done <<< $( tail -n +2 ${file} )
 }
 
 convert_files(){
   echo "* Convert each files into .ogg with max 256kb/s max bit rate"
   mkdir -p "${res_dir}"
   for i in "${tmp_dir}"/*; do 
-    echo "* Converting ${i} file"
     name=${i##*/}
-    echo "   to ${res_dir}/${name%.*}.ogg"
+    echo "* Converting ${name} file to .ogg"
     if ! ffmpeg -hide_banner -loglevel error -i "${i}" -b:a 256000 "${res_dir}/${name%.*}.ogg"; then
       echo "[ffmpeg](fail) ${i}" >> errors.log
     fi
   done
+
+  # clean all zero bytes files
+  find ${res_dir} -name "*" -size 0 -print0 | xargs -0 rm
+
+}
+
+try_mp3(){
+  while read -r f; do
+    name=${f##*/}
+#    echo "* Converting ${name} file to .mp3"
+    echo "* $f"
+    echo "* ${res_dir}/${name%.*}.mp3"
+    if ! ffmpeg -hide_banner -loglevel error -i "${f}" -b:a 256000 "${res_dir}/${name%.*}.mp3"; then
+      echo "[ffmpeg](fail) ${f}" >> errors_try.log
+    fi
+  done <<< $(grep "ffmpeg" ./errors.log | cut -d ' ' -f 2-)
 }
 
 #download
-convert_files
+#convert_files
+try_mp3
